fc847e068f89d8dad63513f568c604f7be2c22d5
05200253 <05200253@D54997.farroupilha.ifrs.edu.br>
2019-08-08 17:40:43 -0300

    nanana

--- PitPlanner/src/pitplanner/FXMLDocumentController.java
+++ PitPlanner/src/pitplanner/FXMLDocumentController.java
@@ -10,7 +10,10 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 /**
  *
@@ -20,12 +23,30 @@ public class FXMLDocumentController implements Initializable {
     
     @FXML
     private Label label;
+    @FXML
+    private TextField nick_field;
+    @FXML
+    private TextField password_field;
+    @FXML
+    private Button login_button;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private void loginButtonAction(ActionEvent event) {
+        label.setText(nick_field.getText() + password_field.getText());
+        
+        if (password_field.getText() != "enter") {
+            Alert dialogoErro = new Alert(Alert.AlertType.ERROR);
+            dialogoErro.setTitle("ERRO");
+            dialogoErro.setHeaderText("Login ou senha inexistentes!");
+            dialogoErro.showAndWait();
+        } else {
+        
+            Alert dialogoErro = new Alert(Alert.AlertType.WARNING);
+            dialogoErro.setTitle("VOCE ENTROU");
+            dialogoErro.showAndWait();
+        }
     }
+    
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
